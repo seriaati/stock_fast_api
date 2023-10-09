37212138877290a8cbcf8a2ab8c7a6966750fd27
seriaati <seria.ati@gmail.com>
2023-10-09 08:14:17 +0800

    feat: return 404 if stock with name not found

--- run_api.py
+++ run_api.py
@@ -28,7 +28,7 @@ async def shutdown():
 
 @app.get("/")
 async def root():
-    return {"message": "Stock Fast API v1.2.0"}
+    return {"message": "Stock Fast API v1.2.1"}
 
 
 @app.get("/history_trades/{stock_id}")
@@ -49,7 +49,10 @@ async def stock_history_trades(stock_id: str, limit: Optional[int] = None):
 @app.get("/stocks")
 async def stocks(name: Optional[str] = None):
     if name:
-        return await Stock.filter(name__icontains=name).values()
+        try:
+            return await Stock.filter(name__icontains=name).values()
+        except DoesNotExist:
+            raise HTTPException(status_code=404, detail="Stock not found")
     return await Stock.all().values()
 
 
